744d9c88fe119dcd38241c342f255a54e38e5459
NonSwag <mrminecraft00@gmail.com>
2022-08-20 12:00:45 +0200

    initial commit

---
+++ src/main/java/net/nonswag/tnl/npc/NPC.java
@@ -0,0 +1,14 @@
+package net.nonswag.tnl.npc;
+
+import net.nonswag.tnl.listener.api.plugin.TNLPlugin;
+
+public class NPC extends TNLPlugin {
+
+    @Override
+    public void enable() {
+    }
+
+    @Override
+    public void disable() {
+    }
+}

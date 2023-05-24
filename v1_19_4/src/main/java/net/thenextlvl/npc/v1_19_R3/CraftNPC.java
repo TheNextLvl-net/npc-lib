0bbbc8101507606351318a2e26d197ff80534f38
NonSwag <mrminecraft00@gmail.com>
2023-05-24 20:58:27 +0200

    added internal methods and fields

--- v1_19_4/src/main/java/net/thenextlvl/npc/v1_19_R3/CraftNPC.java
+++ v1_19_4/src/main/java/net/thenextlvl/npc/v1_19_R3/CraftNPC.java
@@ -1,22 +1,44 @@
 package net.thenextlvl.npc.v1_19_R3;
 
+import com.destroystokyo.paper.profile.CraftPlayerProfile;
 import com.destroystokyo.paper.profile.PlayerProfile;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import net.kyori.adventure.text.Component;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
 import net.thenextlvl.npc.api.Equipment;
 import net.thenextlvl.npc.api.NPC;
 import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_19_R3.CraftWorld;
 
 @Getter
 @Setter
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class CraftNPC implements NPC {
     private Location location;
     private PlayerProfile profile;
     private Component displayName;
     private Equipment equipment;
+    private final ServerPlayer player;
+
+    public CraftNPC(Location location, PlayerProfile profile, Component displayName, Equipment equipment) {
+        this(location, profile, displayName, equipment, new ServerPlayer(
+                MinecraftServer.getServer(),
+                ((CraftWorld) location.getWorld()).getHandle(),
+                ((CraftPlayerProfile) profile).getGameProfile()
+        ));
+    }
+
+    public int getId() {
+        return getPlayer().getId();
+    }
+
+    public int getLoadingRange() {
+        return ((CraftWorld) getLocation().getWorld()).getHandle().spigotConfig.displayTrackingRange;
+    }
 
     @Override
     public NPC clone() {

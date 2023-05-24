6d7c66e93b93b32b5718b3081d2c38a0f91a1b03
NonSwag <mrminecraft00@gmail.com>
2023-05-24 19:54:03 +0200

    added npc factory

---
+++ v1_19_4/src/main/java/net/thenextlvl/npc/v1_19_R3/CraftNPCFactory.java
@@ -0,0 +1,21 @@
+package net.thenextlvl.npc.v1_19_R3;
+
+import com.destroystokyo.paper.profile.PlayerProfile;
+import net.kyori.adventure.text.Component;
+import net.thenextlvl.npc.api.NPC;
+import net.thenextlvl.npc.api.NPCFactory;
+import org.bukkit.Location;
+
+public class CraftNPCFactory implements NPCFactory {
+    @Override
+    public NPC createNPC(Location location, PlayerProfile profile) {
+        return createNPC(location, profile, profile.getName() != null
+                ? Component.text(profile.getName())
+                : Component.empty());
+    }
+
+    @Override
+    public NPC createNPC(Location location, PlayerProfile profile, Component displayName) {
+        return new CraftNPC(location.clone(), profile, displayName, new CraftEquipment());
+    }
+}

67a843f7065c2c79061f353b4984827994673fd9
NonSwag <mrminecraft00@gmail.com>
2023-05-24 19:54:35 +0200

    added new construct method

--- api/src/main/java/net/thenextlvl/npc/api/NPCFactory.java
+++ api/src/main/java/net/thenextlvl/npc/api/NPCFactory.java
@@ -1,6 +1,7 @@
 package net.thenextlvl.npc.api;
 
 import com.destroystokyo.paper.profile.PlayerProfile;
+import net.kyori.adventure.text.Component;
 import org.bukkit.Location;
 
 /**
@@ -15,4 +16,15 @@ public interface NPCFactory {
      * @return the new npc
      */
     NPC createNPC(Location location, PlayerProfile profile);
+
+    /**
+     * Create a new npc object with a custom display name
+     *
+     * @param location the location of the npc
+     * @param profile the profile of the npc
+     * @param displayName the display name of the npc
+     *
+     * @return the new npc
+     */
+    NPC createNPC(Location location, PlayerProfile profile, Component displayName);
 }

94e3e51151d934ff7d995166ea587f72091cd40d
NonSwag <mrminecraft00@gmail.com>
2023-05-24 19:02:24 +0200

    added dependencies

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -16,9 +16,14 @@ version = "1.0.0"
 
 repositories {
     mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.thenextlvl.net/releases")
 }
 
 dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
+    compileOnly("net.thenextlvl.core:annotations:1.0.0")
+}
 
 publishing {
     publications {
